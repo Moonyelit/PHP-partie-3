e715739e140a6eba15871ba3e51b3e6f0fe2f27c
moonye@live.fr <moonye@live.fr>
2024-12-03 16:49:52 +0100

    EXOS fini

--- algo.php
+++ algo.php
@@ -25,10 +25,40 @@
 // }
 
 
-//exo 4
-$exo4 = 1;
+// //exo 4
+// $exo4 = 1;
 
-while ($exo4 < 10) { 
-    echo $exo4 . "<br>"; 
-    $exo4 += $exo4 / 2; 
+// while ($exo4 < 10) { 
+//     echo $exo4 . "<br>"; 
+//     $exo4 += $exo4 / 2; 
+// }
+
+
+//exo 5
+// $exo5 = 0;
+
+// for($exo5 = 0; $exo5 < 15; $exo5 += 1){
+//     echo $exo5 . "<br>" . "On y arrive presque." . "<br>"  ;
+// }
+
+//exo 6
+// $exo6 = 20;
+
+// for($exo6 = 20; $exo6 > 0; $exo6 -= 1){
+//     echo $exo6 . "<br>" . "C'est presque bon." . "<br>"  ;
+// }
+
+// exo 7
+// $exo7 = 1;
+
+// for($exo7 = 1; $exo7 < 100; $exo7 += 15){
+//     echo $exo7 . "<br>" . "On tient le bon bout." . "<br>"  ;
+// }
+
+
+//exo 8
+$exo8 = 200;
+
+for($exo8 = 200; $exo8 > 0; $exo8 -= 12){
+    echo $exo8 . "<br>" . "On tient le bon bout." . "<br>"  ;
 }
